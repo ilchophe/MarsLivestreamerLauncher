da864de71deedb13d0ecd35c8b8d7533966c5015
ilchophe <ilchophe@gmail.com>
2016-03-28 02:21:27 +0200

    First Commit

---
+++ MarsLivestreamerLauncher/MainForm.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace MarsLivestreamerLauncher
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Launchbutton = new System.Windows.Forms.Button();
+            this.Closebutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please enter the twitch channel URL and select stream quality";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Twitch Channel URL";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(67, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Stream Quality";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(148, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(326, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Source (Best)",
+            "High",
+            "Medium",
+            "Low",
+            "Mobile (Worst)"});
+            this.comboBox1.Location = new System.Drawing.Point(148, 67);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(326, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // Launchbutton
+            // 
+            this.Launchbutton.Location = new System.Drawing.Point(138, 112);
+            this.Launchbutton.Name = "Launchbutton";
+            this.Launchbutton.Size = new System.Drawing.Size(120, 23);
+            this.Launchbutton.TabIndex = 5;
+            this.Launchbutton.Text = "Launch Livestreamer";
+            this.Launchbutton.UseVisualStyleBackColor = true;
+            this.Launchbutton.Click += new System.EventHandler(this.Launchbutton_Click);
+            // 
+            // Closebutton
+            // 
+            this.Closebutton.Location = new System.Drawing.Point(285, 112);
+            this.Closebutton.Name = "Closebutton";
+            this.Closebutton.Size = new System.Drawing.Size(75, 23);
+            this.Closebutton.TabIndex = 6;
+            this.Closebutton.Text = "Close";
+            this.Closebutton.UseVisualStyleBackColor = true;
+            this.Closebutton.Click += new System.EventHandler(this.Closebutton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 147);
+            this.Controls.Add(this.Closebutton);
+            this.Controls.Add(this.Launchbutton);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Launch Livestreamer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button Launchbutton;
+        private System.Windows.Forms.Button Closebutton;
+    }
+}
+
---
+++ MarsLivestreamerLauncher/MainForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace MarsLivestreamerLauncher
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Closebutton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex=0;
+        }
+
+        private void Launchbutton_Click(object sender, EventArgs e)
+        {
+            LaunchTwitchStreamer(textBox1.Text,comboBox1.SelectedText);
+        }
+
+        private void LaunchTwitchStreamer(string url, string quality)
+        {
+            ProcessStartInfo p = new ProcessStartInfo("livestreamer.exe");
+            p.Arguments = url + @" " + quality;
+            p.UseShellExecute = true;
+           // Process.Start("livestreamer.exe", ,);
+             Process.Start(p);
+        }
+    }
+}
